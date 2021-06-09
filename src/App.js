02d58805543709d4b45b028379d3146a8f542722
Eric Wise <ericwise@gmail.com>
2021-06-08 21:56:36 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [timePerTicket, SetTimePerTicket] = useState(0);
+  const [floor1, setFloor1] = useState(0);
+  const [floor2, setFloor2] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>Total Time {Number(floor1) + Number(floor2)}</div>
+      <form className="form-inline justify-content-center m-2">
+        <input
+          value={floor1}
+          onChange={(e) => setFloor1(e.target.value)}
+          type="text"
+          placeholder="Floor1"
+          className="form-control"
+        />
+        <input
+          value={floor2}
+          onChange={(e) => setFloor2(e.target.value)}
+          type="text"
+          placeholder="Floor2"
+          className="form-control"
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="btn btn-primary mx-2"
         >
-          Learn React
-        </a>
-      </header>
+          Go
+        </button>
+      </form>
     </div>
   );
 }
